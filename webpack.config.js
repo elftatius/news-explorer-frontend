fb9a9846e7b4edcbaa6b652b1ec9508ac9d00568
Tatyana Tkachenko <elf.tatius@yandex.ru>
2020-11-07 11:20:34 +0600

    webpack configured

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash'); // добавили плагин
+const webpack = require('webpack');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  entry: { main: './src/script/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+// указали путь к файлу, в квадратных скобках куда вставлять сгенерированный хеш
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.css$/,
+        use:  [(isDev ? 'style-loader' : MiniCssExtractPlugin.loader), {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 2,
+          },
+        }, 'postcss-loader'],
+      },
+      // пример настройки плагина image-webpack-loader
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+    ]
+  },
+  plugins: [ 
+    new webpack.DefinePlugin({
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new MiniCssExtractPlugin({
+        filename: 'style.[contenthash].css'
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+              preset: ['default'],
+      },
+      canPrint: true
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new WebpackMd5Hash()
+  ]
+};
