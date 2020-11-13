f79942ead13f29b0f8231b0fc84363ca680ca53b
Tatyana Tkachenko <elf.tatius@yandex.ru>
2020-11-13 23:09:38 +0600

    add signup popup

--- src/script/index.js
+++ src/script/index.js
@@ -1,3 +1,15 @@
 import '../pages/index.css';
 
-console.log('hello');
+const authBtn = document.querySelector('.header__button');
+const closeBtn = document.querySelector('#login-popup-close');
+const loginPopup = document.querySelector('#login-popup');
+
+authBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  loginPopup.classList.add('popup_is-opened');
+});
+
+closeBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  loginPopup.classList.remove('popup_is-opened');
+});
