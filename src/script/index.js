083618a948d7d133d6bac7a70f2f1875c1fe1e82
Tatyana Tkachenko <elf.tatius@yandex.ru>
2020-11-16 22:03:05 +0600

    add mobile popup

--- src/script/index.js
+++ src/script/index.js
@@ -3,6 +3,10 @@ import '../pages/index.css';
 const authBtn = document.querySelector('.header__button');
 const closeBtn = document.querySelector('#login-popup-close');
 const loginPopup = document.querySelector('#login-popup');
+const signUpBtn = document.querySelector('#signup-link');
+const signUpPopup = document.querySelector('#signup-popup');
+const closeSignupBtn = document.querySelector('#signup-popup-close');
+const loginBtn = document.querySelector('#login-link');
 
 authBtn.addEventListener('click', (e) => {
   e.preventDefault();
@@ -12,4 +16,21 @@ authBtn.addEventListener('click', (e) => {
 closeBtn.addEventListener('click', (e) => {
   e.preventDefault();
   loginPopup.classList.remove('popup_is-opened');
+});
+
+signUpBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  loginPopup.classList.remove('popup_is-opened');
+  signUpPopup.classList.add('popup_is-opened');
+});
+
+closeSignupBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  signUpPopup.classList.remove('popup_is-opened');
+});
+
+loginBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  signUpPopup.classList.remove('popup_is-opened');
+  loginPopup.classList.add('popup_is-opened');
 });
