1419b108c93cff93bfc5d10ee8d1cd08f97903b2
Tatyana Tkachenko <elf.tatius@yandex.ru>
2020-11-17 21:44:09 +0600

    rafactoring

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,6 @@ module.exports = {
   },
   rules: {
     'no-underscore-dangle': [2, { allow: ['_id'] }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: ['postcss.config.js'] }],
   },
 };
