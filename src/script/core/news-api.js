8639a905dea23347ed028acb5c81b724543f2aa1
Tatyana Tkachenko <elf.tatius@yandex.ru>
2020-12-02 22:01:36 +0600

    fixed news-api endpoint

--- src/script/core/news-api.js
+++ src/script/core/news-api.js
@@ -1,10 +1,12 @@
 export default class NewsApi {
+  static _endpoint = NODE_ENV === 'development' ? 'https://newsapi.org/v2/everything' : 'https://nomoreparties.co/news/v2/everything';
+
   constructor(token) {
     this._token = token;
   }
 
   getNews(q, articlesPerPage, page) {
-    return (fetch(`https://newsapi.org/v2/everything?apiKey=${this._token}&pageSize=${articlesPerPage}&page=${page}&q=${q}`)
+    return (fetch(`${NewsApi._endpoint}?apiKey=${this._token}&pageSize=${articlesPerPage}&page=${page}&q=${q}`)
       .then((res) => {
         if (!res.ok) {
           return Promise.reject(res.status);
